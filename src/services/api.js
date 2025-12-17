567e5d9e3a75c9778c26703043a6fa65afdd9e0f
Simon Santarelli <simisantarelli@gmail.com>
2025-12-17 00:27:06 -0300

    feat: configurar variable de entorno para la URL de la API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5262/api"
+
 const API = axios.create({
-  baseURL: "http://localhost:5262/api",
+  baseURL: BASE_URL,
 });
 
 // ===== Intercepta token =====
